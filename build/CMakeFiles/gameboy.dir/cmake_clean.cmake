67c373b30294d557af2d55de62dba5c78ed9f9f6
Justin <jwong47@u.rochester.edu>
2025-09-03 13:53:43 -0400

    Removed weird duplicate gameboy.cpp with different run implementations

---
+++ build/CMakeFiles/gameboy.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gameboy.dir/src/cartridge.cpp.o"
+  "CMakeFiles/gameboy.dir/src/cartridge.cpp.o.d"
+  "CMakeFiles/gameboy.dir/src/cpu.cpp.o"
+  "CMakeFiles/gameboy.dir/src/cpu.cpp.o.d"
+  "CMakeFiles/gameboy.dir/src/gameboy.cpp.o"
+  "CMakeFiles/gameboy.dir/src/gameboy.cpp.o.d"
+  "CMakeFiles/gameboy.dir/src/io.cpp.o"
+  "CMakeFiles/gameboy.dir/src/io.cpp.o.d"
+  "CMakeFiles/gameboy.dir/src/main.cpp.o"
+  "CMakeFiles/gameboy.dir/src/main.cpp.o.d"
+  "CMakeFiles/gameboy.dir/src/mbc.cpp.o"
+  "CMakeFiles/gameboy.dir/src/mbc.cpp.o.d"
+  "CMakeFiles/gameboy.dir/src/mmu.cpp.o"
+  "CMakeFiles/gameboy.dir/src/mmu.cpp.o.d"
+  "CMakeFiles/gameboy.dir/src/ppu.cpp.o"
+  "CMakeFiles/gameboy.dir/src/ppu.cpp.o.d"
+  "gameboy"
+  "gameboy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gameboy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
